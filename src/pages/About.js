9680f8308b5bdc9874b32878c3d568ae8abb3073
sankettchauhan <sanket.chauhan4@gmail.com>
2020-12-17 02:12:38 +0530

    completed

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis doloremque ab, accusantium nobis repellat odio impedit magnam eius fugiat recusandae. Nisi aliquam asperiores, reprehenderit fugit facilis rem. Dolor, doloribus dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi reprehenderit facilis nam consequuntur nemo ducimus maxime fugiat sequi? Excepturi quod dolorem, quibusdam nisi dolores doloremque ratione corporis quis quidem commodi!</p>
+    </section>
+  )
+}
+
+export default About
